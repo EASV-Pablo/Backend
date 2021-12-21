c84cdb4bb72fb3bf638e0e4a7ea83782ca18d7bf
EASV-Pablo <pabl0311@easv365.dk>
2021-12-22 00:04:40 +0100

    4

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Backend
         public static void Main(string[] args)
         {
             mqttController = new MqttController();
-            //mongoManager = new MongoManager();
+            mongoManager = new MongoManager();
             //socketManager = new WebSocketManager();
             CreateHostBuilder(args).Build().Run();
         }
