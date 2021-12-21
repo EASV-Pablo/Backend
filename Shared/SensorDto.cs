936904fba8972e1813eeadade4181d7e6ba083f5
EASV-Pablo <pabl0311@easv365.dk>
2021-12-22 00:25:26 +0100

    1

--- Shared/SensorDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Dtos
-{
-    public class SensorDto
-    {
-        public string SensorId { get; set; }
-        public int Value { get; set; }
-        public string Measuretime { get; set; }
-    }
-}
