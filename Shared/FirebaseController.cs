a2438785e6f931c3e2e564f0834d19e17b5d1f68
EASV-Pablo <pabl0311@easv365.dk>
2022-01-04 14:44:57 +0100

    Update FirebaseController.cs

--- Shared/FirebaseController.cs
+++ Shared/FirebaseController.cs
@@ -9,6 +9,7 @@ namespace Backend.Shared
     public class FirebaseController
     {
         String destinationToken = "dW_LZBVjQ0eJjCychkFJs4:APA91bFOL-UZq_-GdvrnKm1AJGEnxsKZVKkcLuW0RNKRhZGycsjA8cObA1hAD2tex0ksIs-a4_7qb1t0Dr35wlm-EWTBAn2k_N2bvnbOvVgRaCmCfbZz6Yy0SpuBYt9u-hYWc_Q_I-r0";
+        String topic = "bike";
         Message message;
 
         public FirebaseController()
@@ -24,7 +25,7 @@ namespace Backend.Shared
             message = new Message()
             {
                 Data = data,
-                Token = destinationToken,
+                Topic = topic,
                 Notification = new Notification()
                 {
                     Title = title,
